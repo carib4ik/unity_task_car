2bd61d691957da8740ff231f8db072e8dfa143eb
carib4ik <83468679+carib4ik@users.noreply.github.com>
2024-06-03 12:30:06 +0800

    Upgrade car movement

--- Assets/Scripts/CarMoveable.cs
+++ Assets/Scripts/CarMoveable.cs
@@ -4,6 +4,7 @@ using UnityEngine.Serialization;
 public class CarMoveable : MonoBehaviour
 {
     [SerializeField] private float _moveSpead;
+    [SerializeField] private float _rotateSpead;
     
     private void Update()
     {
@@ -12,25 +13,31 @@ public class CarMoveable : MonoBehaviour
         
         if (Input.GetKey(KeyCode.W))
         {
-            position.z += step;
+            // position.z += step;
+            transform.Translate(Vector3.forward * step);
         }
         
         if (Input.GetKey(KeyCode.S))
         {
-            position.z -= step;
+            // position.z -= step;
+            transform.Translate(Vector3.back * step);
+
         }
         
         if (Input.GetKey(KeyCode.A))
         {
-            position.x -= step;
+            // position.x -= step;
+            transform.Rotate(0, -_rotateSpead, 0);
+
         }
         
         if (Input.GetKey(KeyCode.D))
         {
-            position.x += step;
+            // position.x += step;
+            transform.Rotate(0, _rotateSpead, 0);
         }
         
-        transform.position = position;
+        // transform.position = position;
 
     }
 }
